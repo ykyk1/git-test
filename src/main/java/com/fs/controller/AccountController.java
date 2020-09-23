2315bfd3230c847f374e023f47a7cf80725aef50
ykyk1 <465056729@qq.com>
2020-09-23 09:05:20 +0800

    test-git first commit

---
+++ src/main/java/com/fs/controller/AccountController.java
@@ -0,0 +1,8 @@
+package com.fs.controller;
+
+/**
+ * @Author lykgogo
+ * @Date 2020/9/23 09:01
+ */
+public class AccountController {
+}
---
+++ src/test/java/com/fs/mapper/AccountMapperTest.java
@@ -0,0 +1,8 @@
+package com.fs.mapper;
+
+/**
+ * @Author lykgogo
+ * @Date 2020/9/23 09:01
+ */
+public class AccountMapperTest {
+}
